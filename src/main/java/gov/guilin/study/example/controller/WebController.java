6be36ff7a12944ef1c192eff6c4885d322787380
dengjili <dengjili@outlook.com>
2020-08-11 22:34:59 +0800

    add

--- src/main/java/gov/guilin/study/example/controller/WebController.java
+++ src/main/java/gov/guilin/study/example/controller/WebController.java
@@ -7,11 +7,15 @@
 package gov.guilin.study.example.controller;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import gov.guilin.study.example.dao.QuestionMapper;
 import gov.guilin.study.example.model.Question;
@@ -36,17 +40,36 @@ public class WebController {
       return "views/home/homepage3.html";
    }
    
-   @RequestMapping(value = "/table/simple")
+   //展示全部列表页面
+   @RequestMapping(value = "/test/list1")
    public String simple(Model model) {
 	   List<Question> data2 = questionMapper.findAll();
 	   model.addAttribute("data2", data2);
-	   return "views/home/homepage4.html";
+	   return "views/test/list1.html";
    }
    
-   @RequestMapping(value = "/form")
+   //新增页面
+   @RequestMapping(value = "/addUI")
    public String form(Model model) {
-	   return "views/home/form.html";
+	   return "views/test/add1.html";
    }
+   
+   //新增题目提交
+   @RequestMapping(value = {"/test/add1" }, method = RequestMethod.POST)
+	public String add(@ModelAttribute Question question) {
+   	// 随机ID
+   	int id = new Random().nextInt(Integer.MAX_VALUE);
+   	question.setId(id);
+   	questionMapper.insert(question);
+   	return "1";
+	}
+   
+   //删除
+   @RequestMapping(value = {"/test/delete1"})
+	public String delete(@RequestParam("id") String id) {
+   	questionMapper.deleteById(id);
+   	return "redirect:/test/list1";
+	}
    
    @RequestMapping(value = "/show")
    public String show(Model model) {
